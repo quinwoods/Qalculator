cc38be0352ee4079061e3bc3567ae4328db4daea
quinwoods <quinwoodz@gmail.com>
2021-01-27 20:31:43 +0000

    Alright somehow your dumbass figured out how to show both lines that need to be added together. Congratufuckinglations.

--- fertilizer.js
+++ fertilizer.js
@@ -3,3 +3,10 @@ function add(a,b) {
 	var sum = parseInt(a) + parseInt(b);
 	document.getElementById("answer").innerHTML = sum;
   }
+  
+ function showa(a){
+	 document.getElementById('line1').innerHTML = '<line x1 = 0 y1 = 0 x2 = ' + a + '  + y2 = 0 style=stroke:rgb(255,0,0);stroke-width:2/>';
+ }
+ function showb(b){
+	  document.getElementById('line2').innerHTML = '<line x1 = 0 y1 = 0 x2 = ' + b + '  + y2 = 0 style=stroke:rgb(255,0,0);stroke-width:2/>';
+  }
